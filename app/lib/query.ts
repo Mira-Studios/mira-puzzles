56c7afe776b8c0343327d51c813eb1f4778847cf
Kolbe Tessarzik <kolbe.tessarzik@gmail.com>
2026-03-26 17:21:56 -0400

    better query param unpacking

--- app/lib/query.ts
+++ app/lib/query.ts
@@ -3,15 +3,11 @@ export function parseParams(params: string): string | object {
         return params;
     }
 
-    let paramList: Array<string> = params.split("=");
-    if (paramList.length % 2) {
-        console.warn(`Goofy query params: '${params}'`);
-        return params;
-    }
-
-    let ret: { [key: string]: any } = {};
-    for (var i = 0; i < paramList.length; i += 2) {
-        ret[paramList[i]] = paramList[i+1];
+    const search = params.startsWith("?") ? params.slice(1) : params;
+    const ret: { [key: string]: any } = {};
+    const sp = new URLSearchParams(search);
+    for (const [key, value] of sp.entries()) {
+        ret[key] = value;
     }
 
     return ret;
