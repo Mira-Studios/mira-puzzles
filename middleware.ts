4b8b30561538fb249ef8541f51659dd8928c64fd
Kolbe Tessarzik <kolbe.tessarzik@gmail.com>
2026-03-28 10:54:12 -0400

    dev login if ?dev=true

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { middleware as appMiddleware } from "./app/middleware";
+
+export const middleware = appMiddleware;
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
